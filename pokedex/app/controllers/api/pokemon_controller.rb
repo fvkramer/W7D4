becdb030ff8e8d101829bc47eeff000db649b96d
fvkramer <krame490@gmail.com>
2018-12-05 15:28:29 -0800

    Integrate react with rails backend - Pokedex

---
+++ pokedex/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,25 @@
+class Api::PokemonController < ApplicationController
+  def index
+    @pokemon = Pokemon.all
+    render :index
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+    render :show
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.save
+      render :show
+    else
+      render json: @pokemon.errors.full_messages
+    end
+  end
+
+  private
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :attack, :defense, :poke_type, :moves, :image_url)
+  end
+end
---
+++ pokedex/app/views/api/pokemon/show.json.jbuilder
@@ -0,0 +1,12 @@
+json.pokemon do 
+  json.extract! @pokemon, :id, :name, :attack, :defense
+  json.moves do 
+    json.array! @pokemon.moves
+  end
+  json.poke_type @pokemon.poke_type
+  json.image_url asset_path(@pokemon.image_url)
+  json.item_ids do 
+    json.array! @pokemon.items.ids
+  end
+end
+
